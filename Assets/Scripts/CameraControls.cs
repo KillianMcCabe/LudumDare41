e6b42e07725d2e660d48af8039a1b676eb1937ee
KillianMcCabe <killianparker93@gmail.com>
2018-04-21 20:20:45 +0100

    Added player/camera input scripts

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+    public GameObject follow;
+	float maxDistance = 24; // TODO: scroll wheel updates this val
+    Vector3 focusOffset;
+    Quaternion cameraRotation;
+    
+    float speed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+        focusOffset = new Vector3(-1, .5f, 0) * maxDistance;
+
+        Vector3 vectorToPlayer = follow.transform.position - transform.position;
+        transform.position = transform.position = follow.transform.position + (Quaternion.LookRotation(Vector3.forward) * focusOffset);
+        transform.rotation = Quaternion.LookRotation(vectorToPlayer.normalized);
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+		float horizontal = 0;
+        float vertical = 0;
+		if (Input.GetButton("Fire3")) {
+			horizontal = Input.GetAxis("CameraHorizontal");
+        	vertical = -Input.GetAxis("CameraVertical");
+		}
+
+        focusOffset = Quaternion.AngleAxis(horizontal * speed * Time.deltaTime, Vector3.up) * focusOffset;
+
+        Vector3 newFocusPole = Quaternion.AngleAxis(vertical * speed * Time.deltaTime, transform.right) * focusOffset;
+        if (Vector3.Dot(Vector3.up, newFocusPole.normalized) <= 0.99f && Vector3.Dot(Vector3.up, newFocusPole.normalized) >= -0.85f) {
+            focusOffset = newFocusPole;
+        }
+        
+		RaycastHit hit;
+        int layerMask = 1 << LayerMask.NameToLayer("Terrain");
+        if (Physics.SphereCast(follow.transform.position, 1f, focusOffset, out hit, focusOffset.magnitude, layerMask)) {
+            transform.position = hit.point + new Vector3(0, 0.2f, 0);
+        } else {
+            transform.position = follow.transform.position + focusOffset;
+        }
+        transform.LookAt(follow.transform);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Camera cam;
+	CharacterController controller;
+
+	float moveSpeed = 8f;
+	Vector3 movement = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        cam = Camera.main;
+        controller = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandleMovementInput();
+		HandleActionInputs();
+    }
+
+    void HandleMovementInput()
+    {
+        // read input
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        // calculate movement vectors
+        Vector3 verticalVector = Vector3.Cross(cam.transform.right, Vector3.up);
+        Vector3 horizontalVector = (new Vector3(cam.transform.right.x, 0, cam.transform.right.z));
+
+        movement += (verticalVector * vertical);
+        movement += (horizontalVector * horizontal);
+        movement = Vector3.ClampMagnitude(movement, 1);
+
+        // turn in direction of movement input
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(movement), 360 * Time.deltaTime);
+		if (Vector3.Dot(transform.forward, movement.normalized) > .9f) {
+			controller.Move(movement * Time.deltaTime * moveSpeed);
+		}
+    }
+
+    void HandleActionInputs()
+    {
+		// flirt
+        if (Input.GetButtonDown("Fire1"))
+        {
+			print("flirt");
+        }
+    }
+}
