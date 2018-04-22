cec409d135497dc9edf623d25e364658cec92263
KillianMcCabe <killianparker93@gmail.com>
2018-04-22 13:45:26 +0100

    Added colliders to Map Generator

--- Assets/Scripts/EditorScripts/MapGenerator.cs
+++ Assets/Scripts/EditorScripts/MapGenerator.cs
@@ -12,6 +12,9 @@ public class MapGenerator : MonoBehaviour {
 	float tileSize = 16;
 
 	public void GenerateMap() {
+		GameObject oldMap = GameObject.Find("Map");
+		DestroyImmediate(oldMap);
+
 		var mapGO = new GameObject();
 		mapGO.name = "Map";
 
@@ -24,5 +27,39 @@ public class MapGenerator : MonoBehaviour {
 				go.transform.SetParent(mapGO.transform);
 			}
 		}
+
+		// add colliders to edges
+		float colliderHeight = 10;
+		var colliderGO1 = new GameObject();
+		colliderGO1.transform.SetParent(mapGO.transform);
+		colliderGO1.name = "collider";
+		colliderGO1.transform.position = new Vector3(0, 0, -((tileSize / 2) * mapHeight));
+		colliderGO1.transform.localEulerAngles = new Vector3(0, 0, 0);
+		BoxCollider boxCollider1 = colliderGO1.AddComponent<BoxCollider>();
+		boxCollider1.size = new Vector3(mapWidth * tileSize, colliderHeight, 1);
+
+		var colliderGO2 = new GameObject();
+		colliderGO2.transform.SetParent(mapGO.transform);
+		colliderGO2.name = "collider";
+		colliderGO2.transform.position = new Vector3(-((tileSize / 2) * mapWidth), 0, 0);
+		colliderGO2.transform.localEulerAngles = new Vector3(0, 90, 0);
+		BoxCollider boxCollider2 = colliderGO2.AddComponent<BoxCollider>();
+		boxCollider2.size = new Vector3(mapWidth * tileSize, colliderHeight, 1);
+
+		var colliderGO3 = new GameObject();
+		colliderGO3.transform.SetParent(mapGO.transform);
+		colliderGO3.name = "collider";
+		colliderGO3.transform.position = new Vector3(0, 0, ((tileSize / 2) * mapHeight));
+		colliderGO3.transform.localEulerAngles = new Vector3(0, 0, 0);
+		BoxCollider boxCollider3 = colliderGO3.AddComponent<BoxCollider>();
+		boxCollider3.size = new Vector3(mapWidth * tileSize, colliderHeight, 1);
+
+		var colliderGO4 = new GameObject();
+		colliderGO4.transform.SetParent(mapGO.transform);
+		colliderGO4.name = "collider";
+		colliderGO4.transform.position = new Vector3(((tileSize / 2) * mapWidth), 0, 0);
+		colliderGO4.transform.localEulerAngles = new Vector3(0, 90, 0);
+		BoxCollider boxCollider4 = colliderGO4.AddComponent<BoxCollider>();
+		boxCollider4.size = new Vector3(mapWidth * tileSize, colliderHeight, 1);
 	}
 }
